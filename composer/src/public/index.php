122e630e6b9c676c739d09330382b4702ffe5b30
RileyManda <rileymanda@RileyMandas-MacBook-Pro.local>
2018-07-02 13:49:33 +0300

    Slim implementation with template view completed

--- composer/src/public/index.php
+++ composer/src/public/index.php
@@ -3,12 +3,11 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../../vendor/autoload.php';
-
 $app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-
+$container =$app->getContainer();
+$container['view'] = new \Slim\Views\PhpRenderer("../templates/");
+$app->get('/', function (Request $request, Response $response) {
+$response = $this->view->render($response, "index.html");
     return $response;
 });
 $app->run();
